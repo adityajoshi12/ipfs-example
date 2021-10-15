dca7f86b91af59446db3db1e8dcaea3675ed5ec3
gauravdattjoshi <letslearncbse0@gmail.com>
2021-10-15 12:50:59 +0530

    storing and retreival

---
+++ app.js
@@ -0,0 +1,49 @@
+const { create } = require("ipfs-http-client");
+const fs = require("fs")
+async function ipfsClient() {
+    const ipfs = await create(
+        {
+            host: "ipfs.infura.io",
+            port: 5001,
+            protocol: "https"
+        }
+    );
+    return ipfs;
+}
+
+
+async function saveText() {
+    let ipfs = await ipfsClient();
+
+    let result = await ipfs.add(`welcome ${new Date()}`);
+    console.log(result);
+}
+// saveText();
+
+async function saveFile() {
+
+    let ipfs = await ipfsClient();
+
+    let data = fs.readFileSync("./package.json")
+    let options = {
+        warpWithDirectory: false,
+        progress: (prog) => console.log(`Saved :${prog}`)
+    }
+    let result = await ipfs.add(data, options);
+    console.log(result)
+}
+saveFile()
+
+async function getData(hash) {
+    let ipfs = await ipfsClient();
+
+    let asyncitr = ipfs.cat(hash)
+
+    for await (const itr of asyncitr) {
+
+        let data = Buffer.from(itr).toString()
+        console.log(data)
+    }
+}
+
+// getData("QmQbA7BrBNkh1bbSgtUYdUJYsHRfvRN6k5vocxHgjadUjr")
